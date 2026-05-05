783f768594ca0bd900b7f5e4e1482adcfaa86f9b
Nestor <nestorhernandezvazquez@gmail.com>
2026-05-04 19:53:14 -0600

    fix: add types in middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   let supabaseResponse = NextResponse.next({ request });
-
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -12,7 +11,7 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options: any }[]) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
           supabaseResponse = NextResponse.next({ request });
           cookiesToSet.forEach(({ name, value, options }) =>
@@ -22,8 +21,6 @@ export async function middleware(request: NextRequest) {
       },
     }
   );
-
-  // Refreshes the session cookie on every request — required by @supabase/ssr
   await supabase.auth.getUser();
   return supabaseResponse;
 }
